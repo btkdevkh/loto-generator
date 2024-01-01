011243ec03aa16d39c028b9aa80b176b122870a5
Bunthoeun Kong <amcjkbtk91@outlook.com>
2024-01-01 19:46:09 +0100

    Lottery Generator

---
+++ main.js
@@ -0,0 +1,129 @@
+// HTML Elements
+const grillNumsEl = document.querySelector(".grill-1-50")
+const startNumsEl = document.querySelector(".grill-1-12")
+const generateBtnEl = document.querySelector(".generate-btn")
+const resultEl = document.querySelector(".result")
+
+// Data
+let nums = []
+let starNums = []
+
+// Functions
+const createHTMLElementNumber = (qty, className, grill) => {
+  for (let i = 1; i <= qty; i++) {
+    const div = document.createElement("div")
+    div.classList.add(className)
+    div.textContent = i
+    grill.append(div)
+  }
+}
+
+const generateNums = () => {
+  let num = Math.floor(Math.random() * 50 + 1)
+
+  while (num === 0) {
+    num = Math.floor(Math.random() * 50 + 1)
+  }
+
+  return num
+}
+
+const generateStarNums = () => {
+  let num = Math.floor(Math.random() * 12 + 1)
+
+  while (num === 0) {
+    num = Math.floor(Math.random() * 12 + 1)
+  }
+
+  return num
+}
+
+const generateLottery = () => {
+  nums = []
+  starNums = []
+
+  for (let i = 1; i <= 5; i++) {
+    const num = generateNums()
+
+    if (nums.includes(num)) {
+      nums.push(generateNums())
+    } else {
+      nums.push(num)
+    }
+  }
+
+  for (let i = 1; i <= 2; i++) {
+    const num = generateStarNums()
+
+    if (starNums.includes(num)) {
+      starNums.push(generateStarNums())
+    } else {
+      starNums.push(num)
+    }
+  }
+
+  Array.from(grillNumsEl.children).forEach((child) => {
+    if (nums.includes(Number(child.textContent))) {
+      child.style.transition = "all .3s"
+      child.style.backgroundColor = "#5a3afd"
+    } else {
+      child.style.backgroundColor = ""
+    }
+  })
+
+  Array.from(startNumsEl.children).forEach((child) => {
+    if (starNums.includes(Number(child.textContent))) {
+      child.style.transition = "all .3s"
+      child.style.backgroundColor = "orangered"
+    } else {
+      child.style.backgroundColor = ""
+    }
+  })
+}
+
+// Events
+createHTMLElementNumber(50, "num", grillNumsEl)
+createHTMLElementNumber(12, "star-num", startNumsEl)
+generateBtnEl.addEventListener("click", () => {
+  resultEl.innerHTML = ""
+  resultEl.style.padding = ""
+
+  generateLottery()
+
+  const timer = setInterval(() => {
+    generateLottery()
+  }, 200)
+
+  setTimeout(() => {
+    clearInterval(timer)
+
+    // Result stuff
+    resultEl.style.padding = "1rem"
+
+    nums
+      .sort((a, b) => a - b)
+      .forEach((num) => {
+        const div = document.createElement("div")
+        div.classList.add("result-flash")
+        div.style.backgroundColor = "#5a3afd"
+        div.textContent = num
+
+        resultEl.append(div)
+      })
+
+    const div = document.createElement("div")
+    div.style.marginLeft = ".5rem"
+    resultEl.append(div)
+
+    starNums
+      .sort((a, b) => a - b)
+      .forEach((num) => {
+        const div = document.createElement("div")
+        div.classList.add("result-flash")
+        div.style.backgroundColor = "orangered"
+        div.textContent = num
+
+        resultEl.append(div)
+      })
+  }, 12000)
+})
